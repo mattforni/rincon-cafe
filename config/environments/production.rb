d529c06f46ab1a7a523d15dcea1db3c8dbb94af2
Matthew <mfornaciari@salesforce.com>
2015-10-07 19:41:14 -0700

    [Mailer] Corrected default host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # Added for Devise.
-  config.action_mailer.default_url_options = {host: 'rincon-coffe.herokuapp.com'}
+  config.action_mailer.default_url_options = {host: 'rincon-coffee.herokuapp.com'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
