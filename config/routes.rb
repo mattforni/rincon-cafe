5f0636b032fde66080bf5d5a4e663052c884346b
Matthew <mfornaciari@salesforce.com>
2015-11-15 20:56:46 -0800

    [Orders] Temporarily removed route and button to orders/new

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
     get 'users/:id', to: 'auth/registrations#show', as: 'user'
   end
 
-  resources :orders, only: [:create, :destroy, :index, :new]
+  # TODO add me back in!!!
+  # resources :orders, only: [:create, :destroy, :index, :new]
+  resources :orders, only: [:create, :destroy, :index]
   get 'orders/last', to: 'orders#last', as: 'orders_last'
 end
 
